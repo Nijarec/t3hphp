92ed2b2f060abb9109dd6630885fc3c134cf109b
DinhHien0307 <dinhhienkhtn@gmail.com>
2017-10-05 14:42:05 +0700

    m_home

---
+++ models/m_home.php
@@ -0,0 +1,32 @@
+<?php
+include_once("database.php");
+class M_home extends database
+{
+	// Hàm hiển thị 4 sản phẩm thông dụng đưa vào slide trong content
+	public function Hien_thi_san_pham()
+	{
+		//câu lệnh sql hiển thị sản phẩm thông dụng mới từ bảng product và newproduct
+		$sql="SELECT * from products p INNER JOIN new_product n on p.ProductID=n.ProductID WHERE n.Priority=1";
+		$this->setQuery($sql);
+		return $this->loadAllRows();
+	}
+	
+	//Hàm hiển thị 8 danh mục sản phẩm chính
+	public function Hien_thi_Category()
+	{
+		$sql="SELECT CategoryName FROM `category` LIMIT 0,8";
+		$this->setQuery($sql);
+		return $this->loadAllRows();
+	}
+	
+	//Hàm hiển thị sản phẩm khuyến mãi (theo promotion price) sort từ thấp lên cao
+	public function Hien_thi_san_pham_theo_promotionprice()
+	{
+		$sql="SELECT * FROM promotion_price pp INNER JOIN products p on p.ProductID=pp.ProductID  
+				ORDER BY `pp`.`Priority`  ASC";
+		$this->setQuery($sql);
+		return $this->loadAllRows();
+	}
+	
+}
+?>
